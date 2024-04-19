8ab5253e142cc95553ef9eaa0fca758d43b91b0e
abdelrahman-ad <abdelrahman.helwa2018@gmail.com>
2024-04-19 16:14:30 +0200

    meddile ware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
-
-export default authMiddleware({});
-
-export const  config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)","/","/(api|trpc)(.*)"],
+ 
+export default authMiddleware();
+ 
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
 
